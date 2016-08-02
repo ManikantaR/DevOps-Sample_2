18c603caca984d557e39099b2133eb652a83080f
ManikantaR <Manikanta.radhakrishna@gmail.com>
2016-08-02 11:27:46 -0400

    Initial Checkin

---
+++ DevOps-Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps_Sample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class WebDowloader
+    {
+       public List<SampleModel> GetModels()
+        {
+            return new List<SampleModel> { new SampleModel {Name="Test",Id=1 } };
+        }
+    }
+
+    public class SampleModel
+    {
+        public string   Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ WebDownloaderTests/DownloaderTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevOps_Sample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDownloader.Tests
+{
+    [TestClass()]
+    public class DownloaderTests
+    {
+        private WebDowloader downloader;
+
+        [TestMethod()]
+        public void GetModels()
+        {
+            downloader = new WebDowloader();
+            var models = downloader.GetModels();
+
+            Assert.IsTrue(models.Count== 1);
+
+            //Assert.Fail();
+        }
+    }
+}
